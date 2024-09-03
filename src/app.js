f20d0e9a1a409743ad81e32805d31116c025101a
Alejandro García Navia <alejogarcia@unicauca.edu.co>
2024-09-03 21:25:51 +0000

    Domain Generator with Bootstrap finalized

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  const LIST = document.getElementById("list");
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        LIST.innerHTML +=
+          '<li class="list-group-item">' +
+          pronoun[p] +
+          adj[a] +
+          noun[n] +
+          ".com" +
+          "</li>";
+      }
+    }
+  }
 };
