4b10fd11eea726914b91b65077ce47997d28b578
Alejandro García Navia <alejogarcia@unicauca.edu.co>
2024-09-04 01:13:27 +0000

    Create list items correctly finalized

--- src/app.js
+++ src/app.js
@@ -3,17 +3,13 @@ window.onload = function() {
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
 
-  const LIST = document.getElementById("list");
   for (let p = 0; p < pronoun.length; p++) {
     for (let a = 0; a < adj.length; a++) {
       for (let n = 0; n < noun.length; n++) {
-        LIST.innerHTML +=
-          '<li class="list-group-item">' +
-          pronoun[p] +
-          adj[a] +
-          noun[n] +
-          ".com" +
-          "</li>";
+        const LIST = document.getElementById("list");
+        let listItem = document.createElement("li");
+        listItem.innerHTML = pronoun[p] + adj[a] + noun[n] + ".com";
+        LIST.appendChild(listItem);
       }
     }
   }
